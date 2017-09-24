c4fd7ed402dc0195fc843548dacac7c02c12a510
Kristen Thyng <kthyng@gmail.com>
2017-09-24 14:44:01 -0500

    fixed script for multiple wind sources and catching when data is messed up

--- plot_sat.py
+++ plot_sat.py
@@ -112,6 +112,16 @@ if plotshipping:
     shape_feature = cartopy.feature.ShapelyFeature(shpreader.Reader(fname).geometries(),
                                     cartopy.crs.PlateCarree(), facecolor='none')
 
+# data locations
+# Galveston Bay Entrance, North Jetty, TX - Station ID: 8771341
+# https://tidesandcurrents.noaa.gov/stationhome.html?id=8771341
+wndbc = [-(94+43.5/60), 29+21.4/60]
+# TABS B
+wtabs = [-(94+53.944/60), 28+58.939/60]
+# currents: g06010
+# https://tidesandcurrents.noaa.gov/cdata/StationInfo?id=g06010#metadata
+tndbc = [-(94+44.450/60), 29+20.533/60]
+
 # Satellite data is in equidistant cylindrical projection which is just lon/lat
 # get this info from "[X] Region and Data Description" on page
 if args.area == 'gcoos':
@@ -278,9 +288,8 @@ for row in soup.findAll('a')[5:]:  # loop through each day
             img = Image.open(io.BytesIO(response.content))
             # img = Image.open(StringIO(response.content))
             foo = np.asarray(img)
-            # if not date == datetime(2017, 1, 8, 17, 5):
+            # if not date == datetime(2015, 12, 3, 17, 20):
             #     continue
-            # import pdb; pdb.set_trace()
             # different processing if RGB vs. embedded scalar values
             if args.var != 'rgb':
                 # mask out bad areas
@@ -364,19 +373,43 @@ for row in soup.findAll('a')[5:]:  # loop through each day
             # plot tide for context
             if plottide:
 
-                # tide
-                base = 'https://tidesandcurrents.noaa.gov/noaacurrents/DownloadPredictions?fmt=csv&i=30min&d='
-                suffix = '&r=2&tz=GMT&u=2&id=g06010_1&t=24hr&i=30min&threshold=leEq&thresholdvalue='
+                # plot tide location
+                ax.plot(tndbc[0], tndbc[1], '^', color='k', markersize=5, transform=pc)
+
+                # tidal prediction (only goes back and forward in time 2 years)
+                # base = 'https://tidesandcurrents.noaa.gov/noaacurrents/DownloadPredictions?fmt=csv&i=30min&d='
+                # suffix = '&r=2&tz=GMT&u=2&id=g06010_1&t=24hr&i=30min&threshold=leEq&thresholdvalue='
+                # url to download data file starting the day before this sat data, week of data
+                # url2 = base + dtstart.strftime('%Y-%m-%d') + suffix
+                ##
                 dtstart = date - timedelta(days=1)
                 dtend = dtstart + timedelta(days=2)
-                # startdatestr = (startdatetime).isoformat()[0:10]
-                # enddatestr = (startdatetime + timedelta(days=2.5)).isoformat()[0:10]
-                # url to download data file starting the day before this sat data, week of data
-                url2 = base + dtstart.strftime('%Y-%m-%d') + suffix
+                # tidal data
+                base = 'https://tidesandcurrents.noaa.gov/cdata/DataPlot?id=g06010&bin=0&bdate='
+                suffix = '&unit=0&timeZone=UTC&view=csv'
+                url2 = base + dtstart.strftime('%Y%m%d') + '&edate=' + dtend.strftime('%Y%m%d') + suffix
                 df = pd.read_csv(url2, parse_dates=True, index_col=0)
                 df = df[:dtend.strftime('%Y%m%d') + ' 12:00'] # go until noon the following day
+                # angle needs to be in math convention for trig and between 0 and 360
+                theta = 90 - df[' Dir (true)']
+                theta[theta<0] += 360
+                # tidal data needs to be converted into along-channel direction
+                # along-channel flood direction (from website for data), converted from compass to math angle
+                diralong = 259
+                diralong = 90 - diralong + 360
+                # direbb, dirflood = 77, 282  # deg true, from tidal prediction website
+                # direbb, dirflood = 90 - direbb, 90 - dirflood + 360  # change from compass to math angles
+                # import pdb; pdb.set_trace()
+                # first convert to east/west, north/south
+                # all speeds in cm/s
+                df['East [m/s]'] = df[' Speed (cm/sec)']*np.cos(np.deg2rad(theta))
+                df['North [m/s]'] = df[' Speed (cm/sec)']*np.sin(np.deg2rad(theta))
+                # then convert to along-channel (mean ebb and mean flood)
+                df['along'] = df['East [m/s]']*np.cos(diralong) - df['North [m/s]']*np.sin(diralong)
 
                 # wind
+                # NDBC data buoy 8771341
+                windname = '8771341'
                 base = 'https://tidesandcurrents.noaa.gov/cgi-bin/newdata.cgi?type=met&id=8771341&begin='
                 suffix = '&units=metric&timezone=GMT&mode=csv&interval=h'
                 url2 = base + dtstart.strftime('%Y%m%d') + '&end=' + dtend.strftime('%Y%m%d') + suffix
@@ -387,15 +420,47 @@ for row in soup.findAll('a')[5:]:  # loop through each day
                 theta = 90 - (dfw[' DIR'] - 180)
                 theta[theta<0] += 360
                 # add to original dataframe
-                df['east'] = dfw[' WINDSPEED']*np.cos(np.deg2rad(theta))
-                df['north'] = dfw[' WINDSPEED']*np.sin(np.deg2rad(theta))
+                dfw['East [m/s]'] = dfw[' WINDSPEED']*np.cos(np.deg2rad(theta))
+                dfw['North [m/s]'] = dfw[' WINDSPEED']*np.sin(np.deg2rad(theta))
+                df = pd.merge(df, dfw[['East [m/s]', 'North [m/s]']], how='outer', left_index=True, right_index=True)
+                # if more than half of the wind entries are nan, use TABS buoys instead
+                # if date == datetime(2017, 3, 2, 18, 55):
+                #     import pdb; pdb.set_trace()
+                if len(dfw[' DIR']) == 0 or np.isnan(dfw[' DIR']).sum() > 0.5*len(dfw[' DIR']):
+                    try:
+                        windname = '42035'
+                        base = 'http://pong.tamu.edu/tabswebsite/subpages/tabsquery.php?tz=UTC&units=M&Buoyname=42035&table=ndbc&datepicker='
+                        suffix = '&Datatype=download&model=False'
+                        url2 = base + dtstart.strftime('%Y-%m-%d') + '+-+' + dtend.strftime('%Y-%m-%d') + suffix
+                        dfw = pd.read_csv(url2, parse_dates=True, index_col=0, delimiter='\t')
+                        dfw = dfw[:dtend.strftime('%Y%m%d') + ' 12:00'] # go until noon the following day
+                        # interpolate to hourly like 8771341
+                        dfw = dfw.resample('60T').interpolate()
+                        # add to original dataframe
+                        df = pd.merge(df, dfw[['East [m/s]', 'North [m/s]']], how='outer', left_index=True, right_index=True)
+
+                        # throw assertion if still too many nans
+                        assert not np.isnan(dfw['East [m/s]']).sum() > 0.5*len(dfw['East [m/s]'])
+                    except:
+                        # backup: TABS buoy B
+                        windname = 'B'
+                        base = 'http://pong.tamu.edu/tabswebsite/subpages/tabsquery.php?tz=UTC&units=M&Buoyname=B&table=met&datepicker='
+                        suffix = '&Datatype=download&model=False'
+                        url2 = base + dtstart.strftime('%Y-%m-%d') + '+-+' + dtend.strftime('%Y-%m-%d') + suffix
+                        dfw = pd.read_csv(url2, parse_dates=True, index_col=0, delimiter='\t')
+                        dfw = dfw[:dtend.strftime('%Y%m%d') + ' 12:00'] # go until noon the following day
+                        # interpolate to hourly like 8771341
+                        dfw = dfw.resample('60T').interpolate()
+                        # add to original dataframe
+                        df = pd.merge(df, dfw[['East [m/s]', 'North [m/s]']], how='outer', left_index=True, right_index=True)
                 df.idx = date2num(df.index.to_pydatetime())  # in units of days
 
 
                 axwind = fig.add_axes([0.51, 0.83, 0.24, 0.08])
                 ddt = 1
-                axwind.quiver(df.idx[::ddt], np.zeros(len(df[::ddt])), df[::ddt]['east'], df[::ddt]['north'], headaxislength=0,
+                axwind.quiver(df.idx[::ddt], np.zeros(len(df[::ddt])), df[::ddt]['East [m/s]'], df[::ddt]['North [m/s]'], headaxislength=0,
                           headlength=0, width=0.2, units='y', scale_units='y', scale=1, color='k')
+                axwind.text(0.75, 0.02, windname, fontsize=6, transform=axwind.transAxes)
                 axwind.get_yaxis().set_ticks(np.arange(-10,15,5))
                 axwind.get_yaxis().set_ticklabels(['', '-5', '', '5', ''])
                 axwind.set_ylim(-10.1,10.1)
@@ -405,10 +470,10 @@ for row in soup.findAll('a')[5:]:  # loop through each day
                 [t.set_visible(False) for t in axwind.get_xticklines()]
 
                 axtide = fig.add_axes([0.51, 0.74, 0.24, 0.08], sharex=axwind)
-                axtide.plot(df.idx, df[' Speed (cm/sec)']/100, color='k')
+                axtide.plot(df.idx, df['along']/100, color='k')
                 axtide.get_yaxis().set_ticks(np.arange(-1,1.5,0.5))
                 axtide.get_yaxis().set_ticklabels(['', '-0.5', '', '0.5', ''])
-                axtide.set_ylim(-1.1, 1.1)
+                axtide.set_ylim(-1.3, 1.3)
                 plt.xticks(rotation=70)
                 axtide.axhline(y=0.0, xmin=0, xmax=1, color='k', linestyle=':', linewidth=0.5)
                 axtide.axvline(x=date2num(date), ymin=0, ymax=1)
